90e87bb4c5e8829ca9ffb964810b2c65f5aea874
callmephilip <philip.nuzhnyy@gmail.com>
2012-12-18 15:41:25 -0800

    utility navigation methods added

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,3 +1,10 @@
+/*
+    Application url mapping
+        - landing 
+        - login (trigger SC connection dialog)
+        - stacks (main application screen)
+*/
+
 define(['backbone'],
     function(Backbone) {
         var ApplicationRouter = Backbone.Router.extend({
@@ -15,6 +22,25 @@ define(['backbone'],
                     Backbone.history.start();
                     this.started = true;
                 }
+            },
+
+            landing : function(){
+                this.navigate("/", {trigger:true});
+            },
+
+            login : function(){
+                this.navigate("/login", {trigger:true});
+            },
+
+            main : function(){
+                this.navigate("/stacks", {trigger:true});
+            },
+
+            /*
+                if the app cannot be run within current browser 
+            */
+            badBrowser : function(){
+                window.location.href = "/oops.html";
             }
 
         });
